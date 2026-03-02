b942dd3756500b6ddeeb849b9253c41090d44026
Damien Ridereau <contact@ridereau.fr>
2026-03-02 11:40:32 +0100

    ✨ Ajout troubleshooting

--- frontend/README.md
+++
@@ -1,2 +0,0 @@
-# README frontend
-modification
--- frontend/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Injection de la variable d'environnement BACKEND_URL dans index.html
-sed -i "s|<head>|<head><script>window.__ENV__={BACKEND_URL:\"${BACKEND_URL:-}\"}</script>|" /app/build/index.html
-
-# Démarrage de serve pour servir l'application sur le port 3000
-npx serve -s /app/build -l 3000
